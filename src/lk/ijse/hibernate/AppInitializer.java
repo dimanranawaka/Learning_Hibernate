d7a6432dc75156051513d1d271874e47014def0b
dimanranawaka <dimanranawaka@gmail.com>
2023-09-04 16:56:36 +0530

    session.get() method

--- src/lk/ijse/hibernate/AppInitializer.java
+++ src/lk/ijse/hibernate/AppInitializer.java
@@ -8,21 +8,21 @@ import org.hibernate.Transaction;
 public class AppInitializer {
     public static void main(String[] args) {
 
-        Customer c1 = new Customer();
-
-
-        c1.setId("C001");
-        c1.setName("Diman");
-        c1.setSalary(50000.00);
-        c1.setAddress("Matara");
-
-        Customer C2 = new Customer();
-
-
-        C2.setId("C002");
-        C2.setName("Diman");
-        C2.setSalary(50000.00);
-        C2.setAddress("Matara");
+//        Customer c1 = new Customer();
+//
+//
+//        c1.setId("C001");
+//        c1.setName("Diman");
+//        c1.setSalary(50000.00);
+//        c1.setAddress("Matara");
+//
+//        Customer C2 = new Customer();
+//
+//
+//        C2.setId("C002");
+//        C2.setName("Diman");
+//        C2.setSalary(50000.00);
+//        C2.setAddress("Matara");
 
 
         // Opening a session
@@ -36,9 +36,10 @@ public class AppInitializer {
 
         /* session.get(<className>,<Identifier>) */
 
-        session.get(Customer.class,"C001");
+        Customer c1 = session.get(Customer.class, "C001");
 
-        System.out.println(session.get(Customer.class,"C001"));
+        System.out.println(c1.getName());
+
 
         // Adding data to the tables
 //        transaction.commit();
