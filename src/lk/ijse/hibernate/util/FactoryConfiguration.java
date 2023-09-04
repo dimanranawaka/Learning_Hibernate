913562b3b8bc34cbe28c1304d8071079062ff768
dimanranawaka <dimanranawaka@gmail.com>
2023-09-04 06:48:26 +0530

    Implement  - Config SessionFactory

--- src/lk/ijse/hibernate/util/FactoryConfiguration.java
+++ src/lk/ijse/hibernate/util/FactoryConfiguration.java
@@ -1,18 +1,39 @@
 package lk.ijse.hibernate.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
 
-     // Create a private static property with name of class
+     // 1. Create a private static property with name of class
      private static FactoryConfiguration factoryConfiguration;
 
-     // Create a private constructor
+     /** 1. Create a private property of SessionFactory */
+     private SessionFactory sessionFactory;
+
+     // 2. Create a private constructor
      private FactoryConfiguration(){
+
+         /* 2. Create a configuration object  */
+          Configuration configuration = new Configuration();
+
+         /* 3. Build a SessionFactory using  created object and assign it to sessionFactory property*/
+          sessionFactory = configuration.buildSessionFactory();
+
      }
 
-     // create a public method to return an instance of the class
+     // 3. create a public method to return an instance of the class
      public static FactoryConfiguration getInstance(){
           return ( (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration()
                   : factoryConfiguration );
+     }
+
+     /** 4. Create a public method to return session */
+     public Session getSession(){
+
+          return sessionFactory.openSession();
+
      }
 
 }
